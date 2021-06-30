841fd31bd5ba727d5868d5f6e18d4c5ee7d2e255
henrik@henrikgieselmann.com <henrik@henrikgieselmann.com>
2021-06-30 10:35:24 +0200

    Added sizeOf project

---
+++ lcpp_sizeOf/lcpp_sizeOf.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "bool: \t\t" << sizeof(bool) << " bytes.\n";
+    std::cout << "char: \t\t" << sizeof(char) << " bytes.\n";
+    std::cout << "short: \t\t" << sizeof(short) << " bytes.\n";
+    std::cout << "int: \t\t" << sizeof(int) << " bytes.\n";
+    std::cout << "long: \t\t" << sizeof(long) << " bytes.\n";
+    std::cout << "float: \t\t" << sizeof(float) << " bytes.\n";
+    std::cout << "double:\t\t" << sizeof(double) << " bytes.\n";    
+}
