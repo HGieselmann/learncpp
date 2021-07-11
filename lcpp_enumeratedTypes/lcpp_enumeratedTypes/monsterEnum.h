6a4d3d793678bfdb7c962b148906cb23687a451e
henrik@henrikgieselmann.com <henrik@henrikgieselmann.com>
2021-07-11 21:39:05 +0200

    Solution for enums

---
+++ lcpp_enumeratedTypes/lcpp_enumeratedTypes/monsterEnum.h
@@ -0,0 +1,12 @@
+﻿#include <string>
+
+enum Monsters : int {
+    monsters_orc,
+    monsters_goblins,
+    monsters_trolls,
+    monsters_ogres,
+    monsters_skeletons,
+    monsters_NUMBER_OF_ITEMS,
+};
+
+std::string printMonsterType(int monster);
